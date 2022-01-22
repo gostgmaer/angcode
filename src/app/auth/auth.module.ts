1b2d7329d37f18bcacd673aaed0a3bfa4814d41f
gostgmaer <kishor81160@gmail.com>
2022-01-22 13:07:46 +0530

    New Login Has been Added

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { ForgetpassComponent } from './forgetpass/forgetpass.component';
+import { SuccessComponent } from './success/success.component';
+import { DeniedComponent } from './denied/denied.component';
+import { DynamicmsgComponent } from './dynamicmsg/dynamicmsg.component';
+
+
+
+@NgModule({
+  declarations: [
+    LoginComponent,
+    SignupComponent,
+    ForgetpassComponent,
+    SuccessComponent,
+    DeniedComponent,
+    DynamicmsgComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class AuthModule { }
---
+++ src/app/auth/dynamicmsg/dynamicmsg.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dynamicmsg',
+  templateUrl: './dynamicmsg.component.html',
+  styleUrls: ['./dynamicmsg.component.css']
+})
+export class DynamicmsgComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
